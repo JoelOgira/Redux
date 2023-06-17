ef57039bf2037453517fb4bb797a5c17294d02d5
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-06-17 22:54:07 +0300

    Modified the component

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,18 +1,24 @@
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postsSlice';
 import PostAuthor from './PostAuthor';
+import TimeAgo from './TimeAgo';
 
 const PostsList = () => {
-    const posts = useSelector(selectAllPosts)
+    const posts = useSelector(selectAllPosts);
+    
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
 
-    const renderedPosts = (posts || []).map(post =>
+    const renderedPosts = (orderedPosts || []).map(post =>
         <article
             key={post?.id}
             className='border p-5 rounded-lg mb-4'
         >
             <h1 className='font-bold capitalize text-4xl pb-4'>{post?.title}</h1>
             <p className='pb-4'>{post?.content.substring(0, 100)}</p>
-            <PostAuthor userId={post?.userId} />
+            <p>
+                <PostAuthor userId={post?.userId} />
+                <TimeAgo timestamp={post?.date}/>
+            </p>
         </article>
     )
 
