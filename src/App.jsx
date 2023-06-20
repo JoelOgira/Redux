7b74d224a607c02908a4b3229e342a803d9fe2f4
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-06-20 21:48:43 +0300

    Added Routes for the rendered components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import Counter from "./features/counter/Counter";
+import Counter2 from "./features/counter2/Counter2";
 import PostsList from "./features/posts/PostsList";
-import AddPostForm from "./features/posts/AddPostForm";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className=" min-h-[100dvh]">
       <div className="container mx-auto pt-10 px-5 md:w-1/2">
         <div className="flex flex-col">
-          <AddPostForm />
-          <PostsList />
+          <Routes>
+            <Route path="/" element={<PostsList />} />
+            <Route path="/count" element={<Counter />} />
+            <Route path="/count2" element={<Counter2 />} />
+          </Routes>
         </div>
       </div>
     </div>
   )
 }
 
-export default App;
+export default App; 
