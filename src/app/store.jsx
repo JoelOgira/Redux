da1338a05efdc92f4ebef5c6167b29e48b10d698
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-06-25 21:59:28 +0300

    Added counter3 Slice

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import postsReducer from '../features/posts/postsSlice';
 import usersReducer from '../features/Users/userSlice';
+import counterReducer2 from '../features/counter2/counterSlice2'
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
         posts: postsReducer,
         users: usersReducer,
+        counter3: counterReducer2,
     }
 })
