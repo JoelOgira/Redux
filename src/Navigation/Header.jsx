11105cb0e9c809f412f2df2cc5ffeb95e8ad1fe0
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-06-30 19:34:58 +0300

    Created Header component

---
+++ src/Navigation/Header.jsx
@@ -0,0 +1,70 @@
+import { Link } from "react-router-dom";
+import { FiAlignRight } from 'react-icons/fi';
+import { useState } from "react";
+
+const Header = () => {
+    const [ isHidden, setIsHidden ] = useState(true);
+    const handleHidden = () => {
+        setIsHidden(!isHidden);
+    }
+
+    return (
+        <div className="top-0 opacity-80 py-8 px-6 w-full bg-[#212529]  border-b-4 border-[#d2691e] md:px-8">
+            <div className="flex flex-row space-x-10 justify-between container mx-auto text-white lg:w-4/6">
+                <div>
+                    < Link to='/windfall' className="font-semibold text-lg hover:border-b-2 border-[#d2691e]">
+                        <span className="text-[#d2691e]">Windfall </span>Technologies
+                    </Link>
+                </div>
+
+                <div>
+                    <div className="flex justify-center items-center">
+                        <button
+                            onClick={handleHidden}
+                            className={`pl-16 sm:hidden`}
+                        >
+                            <FiAlignRight className="text-[#d2691e] text-3xl" />
+                        </button>
+                    </div>
+
+                    {isHidden
+                        ?
+                        null
+                        :
+                        <div className="flex flex-col justify-end items-end pt-4 pr-[0.125rem] space-y-3 sm:hidden">
+                            <Link to="/">
+                                Home
+                            </Link>
+                            <Link to="/posts">
+                                Posts
+                            </Link>
+                            <Link to="/users">
+                                Users
+                            </Link>
+                            <Link to="/counter">
+                                Counter
+                            </Link>
+                        </div>
+                    }
+
+                    <div className="hidden flex-col justify-center items-start space-y-3 sm:flex sm:items-center sm:space-x-5 sm:space-y-0 sm:flex-row">
+                        <Link to="/" className="hover:text-[#d2691e]">
+                            Home
+                        </Link>
+                        <Link to="/posts" className="hover:text-[#d2691e]">
+                            Posts
+                        </Link>
+                        <Link to="/users" className="hover:text-[#d2691e]">
+                            Users
+                        </Link>
+                        <Link to="/counter" className="hover:text-[#d2691e]">
+                            Counter
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
