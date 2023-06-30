0697eb9845874d666680ac37dd759763655cb5a5
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-06-30 19:31:21 +0300

    Created windfall component

---
+++ src/Windfall/Windfall.jsx
@@ -0,0 +1,11 @@
+import Jumbotron from "./Jumbotron";
+
+const Windfall = () => {
+    return (
+        <div className="min-h-[100dvh]">
+            <Jumbotron />
+        </div>
+    )
+}
+
+export default Windfall;
