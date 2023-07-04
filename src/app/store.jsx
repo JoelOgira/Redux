fd5658d0bb68685e96bca77e72d833874bd9599e
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-07-04 21:46:20 +0300

    Modified store file

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,14 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import postsReducer from '../features/posts/postsSlice';
-import usersReducer from '../features/Users/userSlice';
-import counterReducer2 from '../features/counter2/counterSlice2'
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from '../Counter/counterSlice';
+import postsReducer from '../posts/postsSlice';
+import usersReducer from '../users/usersSlice'
 
 export const store = configureStore({
     reducer: {
         counter: counterReducer,
         posts: postsReducer,
         users: usersReducer,
-        counter3: counterReducer2,
     }
-})
+});
+
