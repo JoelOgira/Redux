47651435d9b21ecaf9e9ab59c5ec26866ae1d865
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-07-05 21:20:03 +0300

    Modified the file

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css';
 import { store } from './app/store.jsx';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { fetchUsers } from './users/usersSlice.jsx';
+
+store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
