d7715b7f7a8606d7cdfbf38d15239dc477c08484
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-26 21:46:57 +0300

    Updated the component

--- src/posts/AddPostForm.jsx
+++ src/posts/AddPostForm.jsx
@@ -1,47 +1,53 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { postAdded } from "./postsSlice";
+
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
-    const postRef = useRef();
-
-    const users = useSelector(selectAllUsers);
     const dispatch = useDispatch();
 
     const [ title, setTitle ] = useState('');
     const [ userId, setUserId ] = useState('');
     const [ content, setContent ] = useState('');
+    const [ addRequestStatus, setAddRequestStatus ] = useState('idle');
 
-    useEffect(() => {
-        postRef.current.focus();
-    }, []);
+    const users = useSelector(selectAllUsers);
 
-    const userOptions = (users || []).map(user =>
-        <option key={user?.id} value={user?.id}>
-            {user?.name}
-        </option>
-    )
-
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
+    const canSave = [ title, userId, content ].every(Boolean) && addRequestStatus === 'idle';
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (canSave) {
+            try {
+                setAddRequestStatus('pending')
+                dispatch(addNewPost({ title, body: content, userId })).unwrap()
 
-        dispatch(postAdded(title, userId, content));
-        setTitle('');
-        setUserId('');
-        setContent('');        
+                setTitle('')
+                setContent('')
+                setUserId('')
+            } catch (err) {
+                console.error('Failed to save the post', err)
+            } finally {
+                setAddRequestStatus('idle')
+            }
+        }
     }
 
+    const userOptions = (users || []).map(user =>(
+        <option key={user?.id} value={user?.id}>
+            {user?.name}
+        </option>
+    ))
+
     return (
-        <div>
+        <div className="w-full py-8 px-6 md:px-8">
+        <div className="flex flex-col space-y-6 container mx-auto lg:w-4/6">
             <h2 className="font-bold text-3xl text-[#d2691e] mb-3">Add a New Post</h2>
-            <form onSubmit={handleSubmit} className="flex flex-col space-y-4 mb-2 ">
+            <form onSubmit={handleSubmit} className="w-full flex flex-col space-y-4 mb-2 ">
                 <div className="flex flex-col space-y-1">
                     <label className="text-xl font-semibold" htmlFor="postTitle">Post Title</label>
                     <input
-                        ref={postRef}
                         type="text"
                         required
                         className="w-full p-2 border border-[#d2691e] rounded-md focus:outline-transparent focus:border-none"
@@ -80,6 +86,7 @@ const AddPostForm = () => {
                     Add Post
                 </button>
             </form>
+        </div>
         </div>
     )
 }
