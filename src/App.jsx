d3e1a4db40bc8b7de60a62fa6330a4f425144118
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-26 21:45:59 +0300

    Added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import Header from "./Navigation/Header";
 import Footer from "./Navigation/Footer";
 import CounterDisplay from "./Counter/CounterDisplay";
 import PostsDisplay from "./posts/PostsDisplay";
+import TodoList from "./Todos/TodoList";
+import SinglePostPage from "./posts/SinglePostPage";
+import AddPostForm from "./posts/AddPostForm";
 
 const App = () => {
   return (
-    <div className="min-h-[100dvh] scroll-smooth">
+    <div className="App min-h-[100dvh] scroll-smooth bg-gray-300">
       <Header />
-      <Routes>
-        <Route path="/" element={<Windfall />} />
-        <Route path="/posts" element={<PostsDisplay />} />
-        <Route path="/users" element  />
-        <Route path="/counter" element={<CounterDisplay />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Windfall />} />
+          <Route path="/posts" element={<PostsDisplay />} />
+          <Route path="/new_post" element={<AddPostForm />} />
+          <Route path="/post:id" element={<SinglePostPage />} />
+          <Route path="/todo" element={<TodoList />} />
+          <Route path="/counter" element={<CounterDisplay />} />
+        </Routes>
       <Footer />
     </div>
   )
