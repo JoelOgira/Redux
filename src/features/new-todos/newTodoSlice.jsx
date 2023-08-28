d7d9799a1d8b576e3196a95c023cfadf9715a963
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-08-28 20:47:51 +0300

    Created newtodo slice

---
+++ src/features/new-todos/newTodoSlice.jsx
@@ -0,0 +1,28 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+
+const initialState = {
+    todos: []
+}
+
+const newTodoSlice = createSlice({
+    name: 'newTodo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const todo = {
+                id: nanoid(),
+                text: action.payload,
+            };
+            state.todos.push(todo);
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) =>
+                todo.id !== action.payload
+            )
+        }
+    }
+});
+
+export const { addTodo, deleteTodo } = newTodoSlice.actions;
+
+export default newTodoSlice.reducer;
