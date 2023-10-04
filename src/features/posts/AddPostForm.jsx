87bac537fcc2a0f10097023dc75b40e273172ae3
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-10-04 20:40:25 +0300

    Updated add post form component

--- src/features/posts/AddPostForm.jsx
+++ src/features/posts/AddPostForm.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addNewPost } from '../posts/postsSlice';
 import { selectAllUsers } from "../users/usersSlice";
+import { useNavigate } from "react-router-dom";
 
 const AddPostForm = () => {
     const users = useSelector(selectAllUsers);
+    const navigate = useNavigate();
 
     const [ title, setTitle ] = useState('');
     const [ userId, setUserId ] = useState('');
@@ -13,17 +15,18 @@ const AddPostForm = () => {
 
     const dispatch = useDispatch();
 
-    const canSave = [title, userId, body].every(Boolean) && addRequestStatus === 'idle';
+    const canSave = [ title, userId, body ].every(Boolean) && addRequestStatus === 'idle';
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (canSave) {
             try {
                 setAddRequestStatus('pending');
-                dispatch(addNewPost({title, userId, body})).unwrap();
+                dispatch(addNewPost({ title, userId, body })).unwrap();
                 setTitle('');
                 setUserId('');
                 setBody('');
+                navigate('/');
             } catch (err) {
                 console.error('Failed to save post', err);
             } finally {
@@ -39,51 +42,53 @@ const AddPostForm = () => {
     ));
 
     return (
-        <section className="flex flex-col space-y-3 w-full">
-            <h2 className="text-chocolate italic text-2xl font-semibold">Add New Post</h2>
-            <form onClick={handleSubmit} className="flex flex-col space-y-3">
+        <section className="px-5 flex flex-col space-y-3 justify-start items-start py-8 container mx-auto text-white lg:w-4/6">
+            <div className="flex flex-col space-y-3 w-full">
+                <h2 className="text-chocolate italic text-2xl font-semibold">Add New Post</h2>
+                <form onClick={handleSubmit} className="flex flex-col space-y-3">
 
-                <div className="flex flex-col space-y-1">
-                    <label htmlFor="postTitle">Post Title</label>
-                    <input
-                        type="text"
-                        placeholder="Add Title"
-                        required
-                        className="p-2 rounded focus:outline-none text-black"
-                        value={title}
-                        onChange={e => setTitle(e.target.value)}
-                    />
-                </div>
+                    <div className="flex flex-col space-y-1">
+                        <label htmlFor="postTitle">Post Title</label>
+                        <input
+                            type="text"
+                            placeholder="Add Title"
+                            required
+                            className="p-2 rounded focus:outline-none text-black"
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+                        />
+                    </div>
 
-                <div className="flex flex-col space-y-1">
-                    <label htmlFor="postAuthor">Author</label>
-                    <select
-                        value={userId}
-                        onChange={(e) => setUserId(e.target.value)}
-                        className="text-black rounded p-2 focus-within:outline-none"
+                    <div className="flex flex-col space-y-1">
+                        <label htmlFor="postAuthor">Author</label>
+                        <select
+                            value={userId}
+                            onChange={(e) => setUserId(e.target.value)}
+                            className="text-black rounded p-2 focus-within:outline-none"
 
-                    >
-                        <option value=""></option>
-                        {usersOptions}
-                    </select>
-                </div>
+                        >   
+                            <option value=""></option>
+                            {usersOptions}
+                        </select>
+                    </div>
 
-                <div className="flex flex-col space-y-1">
-                    <label htmlFor="postBody">Post Body</label>
-                    <textarea
-                        cols="30"
-                        rows="6"
-                        placeholder="Add Content"
-                        required
-                        className="p-2 focus:outline-none rounded text-black"
-                        value={body}
-                        onChange={e => setBody(e.target.value)}
-                    />
-                </div>
-                <button disabled={!canSave} className="grid content-center gap-x-5 py-2 px-6 rounded bg-slate-800 mr-auto text-white hover:bg-slate-600">
-                    Save Post
-                </button>
-            </form>
+                    <div className="flex flex-col space-y-1">
+                        <label htmlFor="postBody">Post Body</label>
+                        <textarea
+                            cols="30"
+                            rows="6"
+                            placeholder="Add Content"
+                            required
+                            className="p-2 focus:outline-none rounded text-black"
+                            value={body}
+                            onChange={e => setBody(e.target.value)}
+                        />
+                    </div>
+                    <button disabled={!canSave} className="grid content-center gap-x-5 py-2 px-6 rounded bg-slate-800 mr-auto text-white hover:bg-slate-600">
+                        Save Post
+                    </button>
+                </form>
+            </div>
         </section>
     )
 }
