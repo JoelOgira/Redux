66c6107c2a8ccfc8ecbcb863f97c86b0f1ee2e75
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-10-04 20:42:44 +0300

    Updated the component

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,12 @@ import { store } from './app/store.jsx';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { fetchUsers } from './features/users/usersSlice.jsx';
+import { fetchPosts } from './features/posts/postsSlice.jsx';
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root'); 
+
+store.dispatch(fetchPosts());
 store.dispatch(fetchUsers());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
