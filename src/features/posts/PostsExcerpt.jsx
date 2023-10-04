63f1a294e55793906f69079ea7a1fda94a362fc8
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-10-04 20:38:21 +0300

    Updated posts component

--- src/features/posts/PostsExcerpt.jsx
+++ src/features/posts/PostsExcerpt.jsx
@@ -2,13 +2,15 @@
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
+import { Link } from "react-router-dom";
 
 const PostsExcerpt = ({ post }) => {
     return (
         <article className="flex flex-col space-y-3 border rounded-md p-3">
-            <h2 className="font-semibold italic">{post?.title}</h2>
-            <p>{post?.body.substring(0, 100)}</p>
-            <div className="flex">
+            <h2 className="font-semibold italic text-xl"><Link to={`/post/${post?.id}`}>{post?.title}</Link></h2>
+            <p>{post?.body.substring(0, 75)}...</p>
+            <div className="flex space-x-1">
+                <Link className="underline" to={`/post/${post?.id}`}>View Post</Link>
                 <p><PostAuthor userId={post?.userId} /></p>
                 <p><TimeAgo timestamp={post?.date} /></p>
             </div>
