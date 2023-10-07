edff7bd7b523a8141dac7b85d710e3524bfb077a
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2023-10-07 20:49:37 +0300

    Created UsersPage component

---
+++ src/features/users/UserPage.jsx
@@ -0,0 +1,29 @@
+import { useSelector } from "react-redux";
+import { selectUserById } from "./usersSlice";
+import { selectAllPosts } from "../posts/postsSlice";
+import { Link, useParams } from "react-router-dom";
+
+const UserPage = () => {
+    const { userId } = useParams();
+    const user = useSelector(state => selectUserById(state, Number(userId)));
+
+    const postsForUser = useSelector(state => {
+        const allPosts = selectAllPosts(state)
+        return allPosts.filter(post => post?.userId === Number(userId))
+    });
+
+    const postTitles = postsForUser.map(post => (
+        <li key={post?.id}>
+            <Link to={`/post/${post?.id}`}>{post?.title}</Link>
+        </li>
+    ))
+
+    return (
+        <section className="px-5 flex flex-col space-y-3 justify-start items-start py-8 container mx-auto text-white lg:w-4/6">
+            <h2 className="font-semibold italic text-xl">{user?.name}</h2>
+            <ol className="list-decimal">{postTitles}</ol>
+        </section>
+    )
+}
+
+export default UserPage;
