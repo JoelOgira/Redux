b1b3c8f24b1399068684dec02825825a3f9f3940
Jay <joelogira@gmail.com>
2023-10-13 15:36:49 +0300

    Modified the posts selecting functionality

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,14 +1,11 @@
 import { useSelector } from "react-redux";
-import { selectAllPosts, getPostsStatus, getPostsError } from "./postsSlice";
+import { selectPostIds, getPostsStatus, getPostsError } from "./postsSlice";
 import PostsExcerpt from "./PostsExcerpt";
 
 const PostsList = () => {
-    const posts = useSelector(selectAllPosts);
+    const orderedPostIds = useSelector(selectPostIds);
     const postsStatus = useSelector(getPostsStatus);
     const error = useSelector(getPostsError);
-
-    const uniquePosts = Array.from(new Set(posts.map(post => post.id))).map(id => posts.find(post => post.id === id));
-    const orderedPosts = uniquePosts.slice().sort((a, b) => b.date.localeCompare(a.date));
 
     return (
         <section className="px-5 flex flex-col space-y-3 justify-start items-start py-8 container mx-auto text-white lg:w-4/6">
@@ -17,8 +14,8 @@ const PostsList = () => {
 
                 {!(postsStatus === 'loading') && (postsStatus === 'failed') && <p>{error}</p>}
 
-                {!(postsStatus === 'loading') && !(postsStatus === 'failed') && (postsStatus === 'succeeded') && (orderedPosts || []).map((post) => (
-                    <PostsExcerpt key={post.id} post={post} />
+                {!(postsStatus === 'loading') && !(postsStatus === 'failed') && (postsStatus === 'succeeded') && (orderedPostIds || []).map(postId => (
+                    <PostsExcerpt key={postId} postId={postId} />
                 ))}
             </div>
         </section>
