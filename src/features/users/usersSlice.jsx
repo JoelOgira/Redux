77f9a31164a826646778a06ccafd4c82bbce4f87
Jay <joelogira@gmail.com>
2023-10-13 15:39:31 +0300

    Modified the functionality

--- src/features/users/usersSlice.jsx
+++ src/features/users/usersSlice.jsx
@@ -1,19 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = [
-    { id: "1", name: "Peter Pan" },
-    { id: "2", name: "Hercules" },
-    { id: "3", name: "Pocahontas" }
-]
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async() => {
+    const response = await axios.get(USERS_URL);
+    return response.data;
+});
 
 const userSlice = createSlice({
     name: "users",
     initialState,
-    reducers: {
-
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload;
+        })
     }
 });
 
 export const selectAllUsers = (state) => state.users;
+
+export const selectUserById = (state, userId) =>
+    state.users.find((user) => user.id === userId);
 
 export default userSlice.reducer;
