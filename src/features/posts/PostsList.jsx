025948b0a674ea5039cfe37bbfd10062bbece3bb
Jay <joelogira@gmail.com>
2023-10-20 21:30:21 +0300

    Updated the PostList component

--- src/features/posts/PostsList.jsx
+++ src/features/posts/PostsList.jsx
@@ -1,20 +1,21 @@
 import { useSelector } from "react-redux";
-import { selectPostIds, getPostsStatus, getPostsError } from "./postsSlice";
+import { selectPostIds } from "./postsSlice";
 import PostsExcerpt from "./PostsExcerpt";
+import { useGetPostsQuery } from "./postsSlice";
 
 const PostsList = () => {
+    const { isLoading, isSuccess, isError, error } = useGetPostsQuery();
+
     const orderedPostIds = useSelector(selectPostIds);
-    const postsStatus = useSelector(getPostsStatus);
-    const error = useSelector(getPostsError);
 
     return (
         <section className="px-5 flex flex-col space-y-3 justify-start items-start py-8 container mx-auto text-white lg:w-4/6">
             <div className="w-full grid grid-cols items-center gap-2.5 lg:grid-cols-2 lg:gap-x-5">
-                {(postsStatus === 'loading') && <p>Loading...</p>}
+                {(isLoading) && <p>Loading...</p>}
 
-                {!(postsStatus === 'loading') && (postsStatus === 'failed') && <p>{error}</p>}
+                {!(isLoading) && (isError) && <p>{error}</p>}
 
-                {!(postsStatus === 'loading') && !(postsStatus === 'failed') && (postsStatus === 'succeeded') && (orderedPostIds || []).map(postId => (
+                {!(isLoading) && !(isError) && (isSuccess) && (orderedPostIds || []).map(postId => (
                     <PostsExcerpt key={postId} postId={postId} />
                 ))}
             </div>
